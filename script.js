4f816a643d408286df4a075b83862fbe0a733bcc
Sebastian Amezquita <samezquita2605@gmail.com>
2021-10-12 09:05:17 -0500

    initial commit

--- script.js
+++ script.js
@@ -4,17 +4,21 @@ const toggleIcon = document.getElementById('toggle-icon');
 const img1 = document.getElementById('img1');
 const img2 = document.getElementById('img2');
 const img3 = document.getElementById('img3');
+const img4 = document.getElementById('img4');
 const textBox = document.getElementById('text-box');
+const desc1Box = document.getElementById('desc1');
 
 //Dark or light images
 function imageMode (color) {
-    img1.src = `img/undraw_code_review_${color}.svg`;
+    img1.src = `img/undraw_static_assets_${color}.svg`;
     img2.src = `img/undraw_map_${color}.svg`;
     img3.src = `img/undraw_feeling_proud_${color}.svg`;
+    img4.src = `img/undraw_google_analytics_${color}.svg`;
 }
 
 function toggleDarkLightMode(isDark) {
     nav.style.backgroundColor = isDark ? 'rgb (0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
+    desc1Box.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
     textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
     toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
     isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon'):
